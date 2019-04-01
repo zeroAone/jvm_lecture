8b1238a3d466980eccefa6372ebba43975811bfc
zeroAone <994338972@qq.com>
2019-04-01 16:30:53 +0800

    Update MyTest4.java

--- src/main/java/com/shengsiyuan/jvm/classloader/MyTest4.java
+++ src/main/java/com/shengsiyuan/jvm/classloader/MyTest4.java
@@ -5,8 +5,10 @@ package com.shengsiyuan.jvm.classloader;
  * 动态生成的类型，其父类型是Object
  *
  * 对于数组来说，JavaDoc经常将构成数组的元素为Component，实际上就是讲数组降低一个维度后的类型。
- *
- *
+ *javap -c com.shengsiyuan.jvm.classloader.MyTest4
+ *助记符： 
+ *anewarray :表示创建一个引用类型的（如类、接口、数组）数组，并将其引用值压入栈顶
+ *newarray: 表示创建一个指定的原始类型（如int、float、char等）的数组，并将其引用值压入栈顶
  */
 public class MyTest4 {
     public static void main(String[] args) {
